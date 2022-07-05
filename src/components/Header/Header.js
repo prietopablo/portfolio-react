f13dd28cc7e4a37179ccd2df9285199f4cafda09
PrietoPablo <prietopablo88@gmail.com>
2022-07-05 16:29:02 +0200

    feat: sketching a monopage app

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import './Header.css';
+
+function Header() {
+   return (
+      <div className="header">
+         
+            <h1>Pablo Prieto</h1>
+            <h2>Fullstack JavaScript Developer</h2>
+         
+         <ul>
+            <li>phone : +33619395038</li>
+            <li>mail : prietopablo88@gmail.com</li>
+            <li>location : Asnières-sur-Seine, 92600, France</li>
+         </ul>
+         
+      </div>
+   )
+}
+
+export default Header;
