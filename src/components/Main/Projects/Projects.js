e018d514d6a1392096bd1a34e05b61496562ecbc
PrietoPablo <prietopablo88@gmail.com>
2022-07-05 19:00:11 +0200

    feat; workind on static element to prepare dynamic content

--- src/components/Main/Projects/Projects.js
+++ src/components/Main/Projects/Projects.js
@@ -5,7 +5,18 @@ function Projects() {
     <div className="main-section projects">
       <h3>projects</h3>
       <div className="content">
-         <p>Lorem Ipsum</p>
+         <ul className="date">
+          <li>début</li>
+          <li>fin</li>
+         </ul>
+         <div className="details">
+          <div className="details-title"><h4 className="company">company</h4><span className="location">location</span></div>
+          <ul className="details-points">
+            <li>point point point point point point</li>
+            <li>point point point point point point </li>
+            <li>point point point point point point </li>
+          </ul>
+         </div>
       </div>
     </div>
   );
