8bc2af41b3d5ec77b24f20f1dd50d56fc569c0b8
PrietoPablo <prietopablo88@gmail.com>
2022-08-31 12:49:29 +0200

    resume update

--- src/components/Main/Presentation/Presentation.js
+++ src/components/Main/Presentation/Presentation.js
@@ -9,7 +9,7 @@ function Presentation() {
         <p className="presentation-text">I'm Pablo Prieto, a junior web developer. I imagine, build apps and interfaces for the web.</p>
         <ul className="presentation-links">
           <li>
-            <a href="https://drive.google.com/file/d/1MEj50vNouzATLwIav-ljOdSBYv4yrxwu/view?usp=sharing"><FaScroll /> <span>curriculum vitae</span></a>
+            <a href="https://drive.google.com/file/d/1ywxV7Z2KiSK9BAaG_yen8Ckhpoii7UmI/view?usp=sharing"><FaScroll /> <span>curriculum vitae</span></a>
           </li>
           <li>
             <a href="mailto:prietopablo88@gmail.com"><FaMailBulk /> <span>prietopablo88@gmail.com</span></a>
